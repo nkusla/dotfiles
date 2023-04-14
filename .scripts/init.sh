3f62089ade58c3a3a109c0ed90d22572226e631e
nkusla <nikola.kuslakovic1616@gmail.com>
2023-04-14 21:30:18 +0200

    Fixed init.sh script and updated power menu settings

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -5,30 +5,33 @@ if [ "$EUID" -ne 0 ]; then
 	exit
 fi
 
-HOMEDIR=/home/$SUDO_USER
-mkdir -p $HOMEDIR/Projects
+HOME_DIR=/home/$SUDO_USER
+
+# Creating custom directories
+mkdir -p $HOME_DIR/Projects && chown $SUDO_USER:$SUDO_USER $HOME_DIR/Projects
+mkdir -p $HOME_DIR/Pictures/Screenshots && chown $SUDO_USER:$SUDO_USER $HOME_DIR/Pictures/Screenshots
 
 # Full system upgrade and AUR helper installation
 pacman -Syyu --noconfirm
 pacman -S yay --noconfirm
 
 # Installing from pkglist
-yay -S --noconfirm --needed - < $HOMEDIR/.pkglist/pkglist.txt
-yay -S --noconfirm --needed - < $HOMEDIR/.pkglist/xfce4-pkglist.txt
+yay -S --noconfirm --needed - < $HOME_DIR/.pkglist/pkglist.txt
+yay -S --noconfirm --needed - < $HOME_DIR/.pkglist/xfce4-pkglist.txt
 
 # Copying backgrounds
-cp $HOMEDIR/.backgrounds/background.png /usr/share/backgrounds
-cp $HOMEDIR/.backgrounds/lockscreen.png /usr/share/backgrounds
+cp $HOME_DIR/.backgrounds/background.png /usr/share/backgrounds
+cp $HOME_DIR/.backgrounds/lockscreen.png /usr/share/backgrounds
 
 # Copying lighdm settings
-cp $HOMEDIR/.etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm
+cp $HOME_DIR/.etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm
 
 # Setting up profile picture for lightdm-gtk-greeter
-cp $HOMEDIR/.backgrounds/profile.png $HOMEDIR/.face && chown $SUDO_USER:$SUDO_USER $HOMEDIR/.face
-cp $HOMEDIR/.backgrounds/profile.png /var/lib/AccountsService/icons/$SUDO_USER
-cp $HOMEDIR/.etc/accountsservice.conf /var/lib/AccountsService/users/$SUDO_USER
+cp $HOME_DIR/.backgrounds/profile.png $HOME_DIR/.face && chown $SUDO_USER:$SUDO_USER $HOME_DIR/.face
+cp $HOME_DIR/.backgrounds/profile.png /var/lib/AccountsService/icons/$SUDO_USER
+cp $HOME_DIR/.etc/accountsservice.conf /var/lib/AccountsService/users/$SUDO_USER
 
-# Clearing cache
+# Clearing package cache
 yay -Sc --noconfirm
 
 # Rebooting
